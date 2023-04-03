06b508b9aa1243ddc34175e4f72f7480f186b9d8
unknown <Akib_Rayhan>
2023-04-03 19:05:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { RxBookmark, RxCross2 } from "react-icons/rx";
+import { BsAlarm } from "react-icons/bs";
+import { CgExport } from "react-icons/cg";
+import { HiOutlineArrowTrendingUp } from "react-icons/hi2";
+import { MdTimeline } from "react-icons/md";
+import List from './Components/List';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className="header">
+        <div className="menu">
+          <div className="menu-item">
+            <div style={{ display: 'flex', gap: 4, alignItems: 'center' }}>
+              <RxBookmark className='separate-over-item' />
+              <RxCross2 className='separate-over-item2' />
+            </div>
+          </div>
+          <div className="menu-item">
+            <input type="text" className='inputSearch' />
+          </div>
+          <div className="menu-item">
+
+            <div class="showhim">HOVER ME
+              <div class="showme">
+                <ul>
+                  <li>5m</li>
+                  <li>1m</li>
+                  <li>2w</li>
+                  <li>7d</li>
+                  <li>1d</li>
+                  <li>12h</li>
+                  <li>2h</li>
+                  <li>1h</li>
+                  <li>30min</li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+          <div className="menu-item">
+            1m
+          </div>
+          <div className="menu-item">
+            <HiOutlineArrowTrendingUp />
+          </div>
+          <div className="menu-item">
+            <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
+              <MdTimeline /> Indicator
+            </div>
+          </div>
+          <div className="menu-item" >
+            <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
+              <CgExport /> Export
+            </div>
+          </div>
+          <div className="menu-item">
+            <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
+              <BsAlarm /> Alert
+            </div>
+          </div>
+        </div>
+        <List />
+      </div>
     </div>
   );
 }
